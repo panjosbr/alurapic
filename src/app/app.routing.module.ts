f0f0cf822f5a6847025371845745ccb02eb74601
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2019-07-07 09:38:25 -0300

    Angular pt1 - adicionando component de notfound para rotas nao existentes

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'user/1', component: PhotoListComponent },
   { path: 'user/2', component: PhotoFormComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
