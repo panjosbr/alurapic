755bd956c439469bd8f93dbb2445cd7165ede3a6
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2019-07-07 09:46:46 -0300

    Angular pt1 - usando activatedRoute para pegar param da rota e passar pra service

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,8 +5,8 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: 'user/1', component: PhotoListComponent },
-  { path: 'user/2', component: PhotoFormComponent },
+  { path: 'user/:userName', component: PhotoListComponent },
+  { path: 'user/form', component: PhotoFormComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
