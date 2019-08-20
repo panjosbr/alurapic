841005ec63e6d756ce894fae90bd28c8f13bf8c3
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2019-08-19 23:32:59 -0300

    Criando rotas filhas para a home

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -25,6 +25,7 @@ export class SignInComponent implements OnInit {
       userName: [ '', Validators.required ],
       password: [ '', Validators.required ]
     });
+    this.platformDetector.isPlatformBrowser() && this.userNameInput.nativeElement.focus();
   }
 
   login() {
