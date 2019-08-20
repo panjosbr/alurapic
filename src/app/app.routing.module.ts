a77eb82d2a56dca6a6662cb69ce4c1b9c240c41c
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2019-08-19 23:42:54 -0300

    Habilitando useHash

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -44,7 +44,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(
+      routes,
+      { useHash: true }
+    )
   ],
   exports: [
     RouterModule
