cfa21dcdd61f562fafd0b191158017760cdd3cb8
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2020-05-31 17:03:57 -0300

    Criando componente footer

--- src/app/home/home.routing.module.ts
+++ src/app/home/home.routing.module.ts
@@ -7,26 +7,26 @@ import { SignInComponent } from './signin/signin.component';
 import { SignUpComponent } from './singup/singup.component';
 
 const routes: Routes = [
-    { 
+    {
         path: '',
         component: HomeComponent,
         canActivate: [AuthGuard],
         children: [
-            { 
+            {
                 path: '',
                 component: SignInComponent,
-            }, 
-            { 
+            },
+            {
                 path: 'signup',
                 component: SignUpComponent,
-            },            
+            },
         ]
-    },              
+    },
 ];
 
 @NgModule({
-    imports: [ 
-        RouterModule.forChild(routes) 
+    imports: [
+        RouterModule.forChild(routes)
     ],
     exports: [ RouterModule ]
 })
