4edb80fc4a3ccc3436c5a0644a84d071fd12949c
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2020-06-28 22:34:09 -0300

    Usando injector para injetar serviços fora do construtor

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,18 +1,46 @@
-import { ErrorHandler } from "@angular/core";
-import * as StackTrace from 'stacktrace-js';
+import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { LocationStrategy, PathLocationStrategy } from "@angular/common";
 
+import * as StackTrace from 'stacktrace-js';
+import { UserService } from "../../core/user/user.service";
+
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
 
+  constructor(
+    private injector: Injector,
+  ) {}
+
   handleError(error: any): void {
-    console.log('passei pelo handler', error);
+    console.log('passei pelo handler');
+    const location = this.injector.get(LocationStrategy);
+    const userService = this.injector.get(UserService);
+
+    const url = location instanceof PathLocationStrategy
+      ? location.path()
+      : '';
+
+
+    const message = error.message
+      ? error.message
+      : error.toString();
+
     StackTrace
       .fromError(error)
       .then(stackFrames => {
         const stackAsString = stackFrames
           .map(sf => sf.toString())
           .join('\n');
-        console.log(error);
+
+        console.log(message);
         console.log(stackAsString);
+        console.log('Será enviado para o servidor');
+        console.log({
+          message,
+          url,
+          userName: userService.getUserName(),
+          stackAsString
+        });
       })
   }
 }
